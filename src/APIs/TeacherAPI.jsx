9c54e5259727296dbf7752c82c1252506c9e3d89
Muhammad-AliHaider <107212677+Muhammad-AliHaider@users.noreply.github.com>
2023-04-22 01:55:34 +0500

    Video_Deletion_done

--- src/APIs/TeacherAPI.jsx
+++ src/APIs/TeacherAPI.jsx
@@ -112,3 +112,81 @@ export async function  add_video_to_course (props){
       return response;
 
 }
+
+export async function remove_video_from_course(props){
+  let x = getBaseURL();
+
+  let url = x.toString() + "/teacher/remove_Videos"
+  
+
+  for(let i = 0; i < props.length; i++){
+    // console.log(props[i]);
+  const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        'authorization': window.localStorage.getItem("token")
+        },
+
+      body : JSON.stringify({
+        _id : window.sessionStorage.getItem("CourseID"),
+        VideoID : props[i],
+      })
+    })
+    .then( async (res) =>{
+      //console.log( await res.json() )
+    return await res.json()})
+    .then((res) => {
+      // console.log((res));
+      if(res.error){
+        return res.error;
+      }
+      else{
+        return res.data;
+      }
+    }
+  );
+
+//   if(response.ok){
+//     continue;
+//   }
+//   else{
+//     break;
+//   }
+}
+
+}
+
+export async function  delete_Video(props){
+  let x = getBaseURL();
+
+  let url = x.toString() + "/teacher/delete_Video"
+
+  for(let i = 0; i < props.length; i++){
+
+  const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        'authorization': window.localStorage.getItem("token")
+        },
+
+      body : JSON.stringify({
+        _id : props[i],
+      })
+
+    }).then( async (res) =>{
+      //console.log( await res.json() )
+    return await res.json()})
+    .then((res) => {
+      // console.log((res));
+      if(res.error){
+        console.log("hello this is an ",res.error);
+        return res.error;
+      }
+      else{
+        return res.data;
+      }})
+    }
+    window.location.href = "/student/course";
+}
