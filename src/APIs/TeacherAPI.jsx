ddb9adffc1c0da33893dcc3c1fadfe9235e4fbe9
Muhammad-AliHaider <107212677+Muhammad-AliHaider@users.noreply.github.com>
2023-05-06 09:33:00 +0500

    Revert "Revert "Quiz_Add_and_Edition_and Deletion done""
    This reverts commit c0008a106eae6548cf857b333b331cdedf0c2a12.

--- src/APIs/TeacherAPI.jsx
+++ src/APIs/TeacherAPI.jsx
@@ -210,12 +210,6 @@ export async function remove_video_from_course(props){
 //   else{
 //     break;
 //   }
-//   if(response.ok){
-//     continue;
-//   }
-//   else{
-//     break;
-//   }
 }
 
 }
